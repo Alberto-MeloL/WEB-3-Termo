e0b536c6c173d0ecec95a5f1360041517ba5d07c
Alberto-MeloL <albertomeloteco@gmail.com>
2024-03-06 16:55:27 -0300

    ok

--- pizzaria-senia/src/app/app.component.ts
+++ pizzaria-senia/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
+import { PromocoesSemanalComponent } from './view/promocoes-semanal/promocoes-semanal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, PromocoesSemanalComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
