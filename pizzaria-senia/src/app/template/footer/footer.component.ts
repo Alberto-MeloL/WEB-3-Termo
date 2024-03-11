137bc28d220154b346d73949d461c49b0aed4818
Dev_Alberto <albertomeloteco@gmail.com>
2024-03-10 23:07:14 -0300

    ok

--- pizzaria-senia/src/app/template/footer/footer.component.ts
+++ pizzaria-senia/src/app/template/footer/footer.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
+
 export class FooterComponent {
+  // estrelas = [1,2,3,4,5]; //representando as estrelas
+  avaliacaoSelecionada = 0; //ou seja nenhuma
+  // avaliacao = document.querySelectorAll('.estrela-icone')
 
-  
-  getAvaliacao(dataAvaliacao: number){//recebendo o atributa data-avaliacao
-    const estrelaSelecionada = document.querySelector('.estrela-icone');
-    const getDataAvaliacao = estrelaSelecionada?.getAttribute('data-avaliacao')
+  getAvaliacao(index: number):void {
+    this.avaliacaoSelecionada = index; //qual foi selecionada
+    console.log('Indíce:', index)
 
-    
-  
-  
+    // if (this.avaliacaoSelecionada != -1) {
+    //   this.estrelas[index].
+    // }
   
   }
 }
