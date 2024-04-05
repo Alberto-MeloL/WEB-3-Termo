b74b6b0c5e29c06b53061a9bddd7341a4771ae95
Dev_Alberto <albertomeloteco@gmail.com>
2024-04-05 19:11:48 -0300

    ok

--- pizzaria-senia/src/app/view/cardapio/cardapio.component.ts
+++ pizzaria-senia/src/app/view/cardapio/cardapio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PedidoService } from '../../controller/service/pedidos/pedido.service';
 
 @Component({
   selector: 'app-cardapio',
@@ -9,5 +10,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './cardapio.component.css'
 })
 export class CardapioComponent {
-
+constructor(public pedidoService: PedidoService){}
 }
--- pizzaria-senia/src/app/view/login/login.component.ts
+++ pizzaria-senia/src/app/view/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { LoginServiceService } from '../../controller/service/login/login-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,23 +16,28 @@ export class LoginComponent {
   constructor(
     private loginService: LoginServiceService,
     private formBuilder: FormBuilder,
+    private router: Router
   ) {
-
     this.formCliente = this.formBuilder.group({
-email: [''],
-senha: ['']
+      email: [''],
+      senha: [''],
     });
   }
 
   //método login
   onSubmit(event: Event) {
-    event.preventDefault()
+    event.preventDefault();
     //esses parametros estão sendo passados para o service
     const { email, senha } = this.formCliente.value;
     this.loginService.realizarLogin(email, senha).subscribe({
       //respostas
-      next: (res) => console.log('Login realizado com sucesso!', res),
-      error: (err) => console.log('Falha ao realizar login', err),
+      next: (res) => {
+        this.router.navigate(['/']);//lembrar de fazer rotas coringa, e emitir uma mensagem
+        console.log('Login realizado com sucesso!', res);
+      },
+      error: (err) => {
+        console.log('Falha ao realizar login', err);
+      },
     });
   }
 }
